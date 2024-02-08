cd21391ff69bc84423cf9a9611b9c9eb7d3fce52
Kristiyan Stefanov <kristianstefanof@gmail.com>
2024-02-08 18:57:20 +0200

    deploy app

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
-import { disableReactDevTools } from "@/fvilers/disable-react-devtools";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if(process.env.NODE_ENV === "production") disableReactDevTools();
 
